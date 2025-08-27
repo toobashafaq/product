59b0523e3d5f985f8a199bf0b74e782dbde2786f
toobashafaq <toobashafaq5@gmail.com>
2025-08-27 20:37:04 +0530

    create product

--- src/main/java/com/application/product/product/controller/ProductController.java
+++ src/main/java/com/application/product/product/controller/ProductController.java
@@ -1,5 +1,12 @@
 package com.application.product.product.controller;
 
+import com.application.product.product.dto.ProductDTO;
+import com.application.product.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/product")
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO){
+        return new ResponseEntity<>(productService.createProduct(productDTO), HttpStatusCode.valueOf(201));
+    }
 
 }
--- src/main/java/com/application/product/product/service/ProductService.java
+++ src/main/java/com/application/product/product/service/ProductService.java
@@ -1,8 +1,36 @@
 package com.application.product.product.service;
 
+import com.application.product.product.dto.ProductDTO;
+import com.application.product.product.entity.Category;
+import com.application.product.product.entity.Product;
+import com.application.product.product.mapper.ProductMapper;
+import com.application.product.product.repository.CategoryRepository;
 import com.application.product.product.repository.ProductRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class ProductService {
+    private ProductRepository productRepository;
+    private CategoryRepository categoryRepository;
+
+    public ProductDTO createProduct(ProductDTO productDTO){
+        /**
+         * id,name,descp,price,categoryId
+         **/
+
+        //check category is available or not
+        Category category=categoryRepository.findById(productDTO.getCategoryId())
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+
+//        DTO TO ENTITY
+
+        Product product=ProductMapper.toProductEntity(productDTO,category);
+        product =productRepository.save(product);
+        //return entity to dto
+        return ProductMapper.toProductDTO(product);
+
+
+    }
 }
