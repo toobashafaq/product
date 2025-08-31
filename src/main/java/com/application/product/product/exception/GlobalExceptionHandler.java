f9a4831c1d9bd18c26a38769c99f524f178592bb
toobashafaq <toobashafaq5@gmail.com>
2025-08-31 11:49:49 +0530

    Global exceptional handler

--- src/main/java/com/application/product/product/exception/GlobalExceptionHandler.java
+++ src/main/java/com/application/product/product/exception/GlobalExceptionHandler.java
@@ -1,20 +1,46 @@
 package com.application.product.product.exception;
 
+import com.application.product.product.dto.ExceptionResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CategoryAlreadyExistsException.class)
-    public ResponseEntity<String> handleCategoryAlreadyExistsException(CategoryAlreadyExistsException e){
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    public ResponseEntity<ExceptionResponseDTO> handleCategoryAlreadyExistsException(CategoryAlreadyExistsException e, WebRequest webRequest){
+        ExceptionResponseDTO exceptionResponseDTO=new ExceptionResponseDTO(
+                webRequest.getDescription(false),HttpStatus.CONFLICT,
+                e.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exceptionResponseDTO);
     }
 
     @ExceptionHandler(CategoryNotFoundException.class)
-    public ResponseEntity<String> handleCategoryNotFound(CategoryNotFoundException e){
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    public ResponseEntity<ExceptionResponseDTO> handleCategoryNotFound(CategoryNotFoundException e,WebRequest webRequest){
+            ExceptionResponseDTO exceptionResponseDTO=new ExceptionResponseDTO(
+                    webRequest.getDescription(false),HttpStatus.NOT_FOUND,
+                    e.getMessage(),
+                    LocalDateTime.now()
+            );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionResponseDTO);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponseDTO> handleCategoryNotFound(Exception e,WebRequest webRequest){
+        ExceptionResponseDTO exceptionResponseDTO=new ExceptionResponseDTO(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                e.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exceptionResponseDTO);
     }
 }
