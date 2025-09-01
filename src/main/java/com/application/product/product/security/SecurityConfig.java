f6b873e4c03ed68089dd1ca90f11df1674fbcaf9
toobashafaq <toobashafaq5@gmail.com>
2025-09-01 20:41:58 +0530

    basic authentication

---
+++ src/main/java/com/application/product/product/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.application.product.product.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf->csrf.disable()).
+                authorizeHttpRequests(request-> {
+                    request.requestMatchers(HttpMethod.GET, "/api/**").permitAll();
+                    request.anyRequest().authenticated();
+                }).httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails userDetails= User.builder()
+                .password(passwordEncoder().encode("admin"))
+                .username("admin")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails seller=User.builder()
+                .password(passwordEncoder().encode("seller"))
+                .username("seller")
+                .roles("Seller")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails,seller);
+    }
+}
