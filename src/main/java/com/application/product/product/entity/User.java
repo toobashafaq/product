e74195a04efda2b375dbf6c1133b8e82e6535ef8
toobashafaq <toobashafaq5@gmail.com>
2025-09-03 20:16:44 +0530

    JWT authentication

---
+++ src/main/java/com/application/product/product/entity/User.java
@@ -0,0 +1,17 @@
+package com.application.product.product.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "Users")
+public class User {
+
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+}
