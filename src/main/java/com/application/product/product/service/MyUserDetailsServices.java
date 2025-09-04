827bcf9f51c046c015d457f04557c3ae7f1e0bbf
toobashafaq <toobashafaq5@gmail.com>
2025-09-04 20:19:01 +0530

    JWT authentication

--- src/main/java/com/application/product/product/service/MyUserDetailsServices.java
+++ src/main/java/com/application/product/product/service/MyUserDetailsServices.java
@@ -1,4 +1,11 @@
 package com.application.product.product.service;
 
+import com.application.product.product.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MyUserDetailsServices {
+    public User createUser(User user){
+
+    }
 }
